e16474cdc5fc9d3721fdd6cb18747c7d16e048e4
KateBilous <katebilous@outlook.com>
2016-10-28 23:46:46 +0300

    added PageObject

---
+++ src/main/java/pages/CreateIssue.java
@@ -0,0 +1,73 @@
+package pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
+
+public class CreateIssue {
+    String issueKey ="";
+    private WebDriver driver;
+
+
+    public CreateIssue(WebDriver driver) {this.driver = driver;}
+
+    public void createBug(){
+       // WebElement createButton = (new WebDriverWait(driver, 10))
+              //  .until(ExpectedConditions.presenceOfElementLocated(By.id("create_link")));
+      // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id=\"create_link\"]")).click();
+        driver.findElement(By.xpath("//*[@id='issuetype-field']")).clear();
+        driver.findElement(By.xpath("//*[@id='issuetype-field']")).sendKeys("Bug");
+        WebElement summary = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"summary\"]")));
+    }
+     public void createSummary(){
+         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+         driver.findElement(By.xpath("//*[@id='summary']")).clear();
+         driver.findElement(By.xpath("//*[@id='summary']")).sendKeys(" This summary was created via WebDriver");
+         //WebElement assignee = driver.findElement(By.xpath("//*[@id=\"assignee-container\"]/label"));
+         WebElement assignee = (new WebDriverWait(driver, 10))
+                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"assignee-container\"]/label")));
+
+     }
+    public void createAssignee(){
+       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id='assignee-field']")).clear();
+        driver.findElement(By.xpath("//*[@id='assignee-field']")).sendKeys("katherinebilous", Keys.ENTER);
+      //  WebElement createButton = driver.findElement(By.xpath("//*[@id='create_link']"));
+        WebElement createButton = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='create_link']")));
+
+
+    }
+    public void  getIssueKey(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        issueKey = driver
+                .findElement(By.xpath("//*[@id=\"aui-flag-container\"]/div/div/a"))
+                .getAttribute("data-issue-key");
+        System.out.println(issueKey);
+    }
+    public void deleteIssue(){
+        driver.get("http://soft.it-hillel.com.ua:8080/browse/" + issueKey);
+        driver.findElement(By.xpath("//*[@id='opsbar-operations_more']/span[1]")).click();
+        driver.findElement(By.xpath("//*[@id='delete-issue']/span")).click();
+        driver.findElement(By.xpath("//*[@id='delete-issue-submit']")).click();
+
+
+    }
+
+
+
+
+
+
+
+}
--- src/test/java/JiraUITests/JiraUI.java
+++ src/test/java/JiraUITests/JiraUI.java
@@ -4,19 +4,23 @@ package JiraUITests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import pages.CreateIssue;
+import pages.LoginPage;
 
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
 public class JiraUI {
-    String created_issue="";
+    String issueKey ="";
 
 
     protected WebDriver driver;
@@ -26,6 +30,18 @@ public class JiraUI {
         driver= new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://soft.it-hillel.com.ua:8080/login.jsp");
+
+        //Explicit Waits
+        WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("header")));
+
+        LoginPage loginPage= new LoginPage(driver);
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+        loginPage.clickLogin();
+
+
+
     }
     @AfterTest
     public  void tearDown()
@@ -34,51 +50,66 @@ public class JiraUI {
     }
     @Test
     public void jiraLogin() throws InterruptedException {
-
-
-
-
         driver.manage().window().maximize();
-        driver.findElement(By.id("login-form-username")).sendKeys("katherinebilous");
-        driver.findElement(By.id("login-form-password")).sendKeys("Polis484)");
-        driver.findElement(By.id("login-form-submit")).click();
 
-            sleep(3000);
+
+        LoginPage loginPage= new LoginPage(driver);
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+        loginPage.clickLogin();
+
+
+
+
+
+
+
+
+
+      //  driver.findElement(By.id("login-form-username")).sendKeys("katherinebilous");
+      //  driver.findElement(By.id("login-form-password")).sendKeys("Polis484)");
+      //  driver.findElement(By.id("login-form-submit")).click();
+
 
 
 
 
     }
-    @Test(dependsOnMethods = {"jiraLogin"})
-    public void createIssue() throws InterruptedException {
+    @Test
+    public void createIssue()  {
 
         driver.manage().window().maximize();
-        driver.findElement(By.xpath("//*[@id=\"create_link\"]")).click();
-        driver.findElement(By.xpath("//*[@id='issuetype-field']")).clear();
-        driver.findElement(By.xpath("//*[@id='issuetype-field']")).sendKeys("Bug");
-
-            sleep(1000);
-
-        driver.findElement(By.xpath("//*[@id='summary']")).clear();
-        sleep(3000);
-        driver.findElement(By.xpath("//*[@id='summary']")).sendKeys(" This summary was created via WebDriver");
-        driver.findElement(By.xpath("//*[@id='assignee-field']")).clear();
-        sleep(3000);
-        driver.findElement(By.xpath("//*[@id='assignee-field']")).sendKeys("katherinebilous", Keys.ENTER);
+        LoginPage loginPage= new LoginPage(driver);
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+        loginPage.clickLogin();
 
 
+        CreateIssue createIssue = new CreateIssue(driver);
+        createIssue.createBug();
+        createIssue.createSummary();
+        createIssue.createAssignee();
+        createIssue.getIssueKey();
 
-            sleep(3000);
-        created_issue = driver
+        createIssue.deleteIssue();
+
+
+
+
+
+       /*
+        issueKey = driver
                 .findElement(By.xpath("//*[@id=\"aui-flag-container\"]/div/div/a"))
                 .getAttribute("data-issue-key");
-        System.out.println(created_issue);
+        System.out.println(issueKey);*/
 
 
     }
     @Test (dependsOnMethods = {"jiraLogin", "createIssue"})
     public void deleteIssue() throws InterruptedException {
-         driver.get("http://soft.it-hillel.com.ua:8080/browse/" +created_issue);
+        CreateIssue createIssue =new CreateIssue(driver);
+        createIssue.deleteIssue();
+         /*driver.get("http://soft.it-hillel.com.ua:8080/browse/" + issueKey);
         sleep(5000);
 
         driver.findElement(By.xpath("//*[@id='opsbar-operations_more']/span[1]")).click();
@@ -87,8 +118,14 @@ public class JiraUI {
         driver.findElement(By.xpath("//*[@id='delete-issue']/span")).click();
         sleep(5000);
         driver.findElement(By.xpath("//*[@id='delete-issue-submit']")).click();
-        sleep(3000);
+        sleep(3000);*/
+    }
+    @Test
+    public void updateReporter(){
+
     }
 
 
 }
+
+
