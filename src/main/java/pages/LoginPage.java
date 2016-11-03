4a0c470868ff7ab301c73fab1c4fe8adec1732b4
Arthur <a.a.piluck@gmail.com>
2016-11-03 09:27:32 +0200

    added @Step support

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,6 +3,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import ru.yandex.qatools.allure.annotations.Step;
 
 public class LoginPage {
     private WebDriver driver;
@@ -11,13 +12,15 @@ public class LoginPage {
         this.driver = driver;
     }
 
-
+    @Step("Enter user name")
     public void enterUsername() {driver.findElement(By.id("login-form-username")).sendKeys("katherinebilous");}
 
+    @Step("Enter user password")
     public void enterPassword() {
         driver.findElement(By.id("login-form-password")).sendKeys("Polis484)");
     }
 
+    @Step("Click login")
     public void clickLogin() {
         driver.findElement(By.id("login-form-submit")).click();
     }
